0f991c03b2595a8955c7a1e7bb6f53c9187a21b2
Oleg Kornitskiy <iam@kornitskiy.ru>
2026-05-29 12:19:31 +0500

    add qstyle

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include "MainWindow.h"
 
 #include <QApplication>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QFile styleFile("://style/style.qss"); // путь из ресурсов
+    if (styleFile.open(QFile::ReadOnly)) {
+        a.setStyleSheet(styleFile.readAll());
+        styleFile.close();
+        qDebug() << "ok";
+    }
+
     MainWindow w;
     w.show();
     return a.exec();
