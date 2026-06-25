b5dcd496fb2574d587851361efcaca1a3e9bd7e1
Oleg Kornitskiy <iam@kornitskiy.ru>
2026-06-25 12:40:13 +0500

    add label to constant and counter

--- field_editor_factory.cpp
+++ field_editor_factory.cpp
@@ -1,27 +1,31 @@
 #include "field_editor_factory.h"
+#include <QLabel>
 #include <QSpinBox>
 #include "integer_packet_field.h"
 
 QWidget *FieldEditorFactory::createEditor(BasePacketField &field, QWidget *parent)
 {
-    if (auto *intField = dynamic_cast<IntegerPacketField *>(&field)) {
-        auto *spinBox = new QSpinBox(parent);
-        spinBox->setRange(intField->min(), intField->max());
-        spinBox->setValue(intField->value());
+    if (field.source() == FieldSource::Input) {
+        if (auto *intField = dynamic_cast<IntegerPacketField *>(&field)) {
+            auto *spinBox = new QSpinBox(parent);
+            spinBox->setRange(intField->min(), intField->max());
+            spinBox->setValue(intField->value());
 
-        QObject::connect(spinBox,
-                         QOverload<int>::of(&QSpinBox::valueChanged),
-                         [intField](int newValue) { intField->setValue(newValue); });
+            QObject::connect(spinBox,
+                             QOverload<int>::of(&QSpinBox::valueChanged),
+                             [intField](int newValue) { intField->setValue(newValue); });
 
-        // if dont input -- disabled.
-        if (intField->source() != FieldSource::Input) {
-            spinBox->setEnabled(false);
+            return spinBox;
         }
-        return spinBox;
+
+        // TO DO:
+        // if (auto* floatField = dynamic_cast<FloatField*>(&field)) { ... }
+    } else if (field.source() == FieldSource::Constant) {
+        auto label = new QLabel(QString("Const"));
+        return label;
+    } else if (field.source() == FieldSource::Counter) {
+        auto label = new QLabel(QString("Counter"));
+        return label;
     }
-
-    // TO DO:
-    // if (auto* floatField = dynamic_cast<FloatField*>(&field)) { ... }
-
     return nullptr;
 }
